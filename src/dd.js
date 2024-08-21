4bc9ecd9f92b0443cf544e7c7c4579dbc9eb2e95
Rod Ebrahimi <rodebrahimi@gmail.com>
2024-08-21 14:09:15 -0700

    small fixes and fade menu

--- src/dd.js
+++ src/dd.js
@@ -1,20 +1,44 @@
+// Mobile menu functionality
+const mobileMenu = document.querySelector('#mobile-menu');
 const mobileMenuButton = document.querySelector('#mobile-menu-button');
 const closeMenuButton = document.querySelector('#close-menu-button');
-const mobileMenu = document.querySelector('#mobile-menu');
 
-if (mobileMenuButton && closeMenuButton && mobileMenu) {
+if (!mobileMenu || !mobileMenuButton || !closeMenuButton) {
+    console.error('menu html elements missing');
+} else {
     const toggleMobileMenu = () => {
-        mobileMenu.classList.toggle('active');
-        document.body.classList.toggle('menu-open');
+        mobileMenu.classList.toggle('hidden');
+        document.body.classList.toggle('overflow-hidden');
     };
 
-    mobileMenuButton.addEventListener('click', toggleMobileMenu);
-    closeMenuButton.addEventListener('click', toggleMobileMenu);
+    mobileMenuButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        toggleMobileMenu();
+    });
+
+    closeMenuButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        toggleMobileMenu();
+    });
 
     mobileMenu.querySelectorAll('a').forEach(link => 
         link.addEventListener('click', toggleMobileMenu)
     );
-
-} else {
-    console.error('elements not found');
 }
+
+const header = document.querySelector('#main-header');
+let lastScrollY = window.scrollY;
+
+window.addEventListener('scroll', () => {
+    if (mobileMenu.classList.contains('hidden')) {
+        const currentScrollY = window.scrollY;
+        
+        if (currentScrollY > lastScrollY && currentScrollY > 75) {
+            header.classList.add('-translate-y-full', 'opacity-0');
+        } else {
+            header.classList.remove('-translate-y-full', 'opacity-0');
+        }
+
+        lastScrollY = currentScrollY;
+    }
+});
